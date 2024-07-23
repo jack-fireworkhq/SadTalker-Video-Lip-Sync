c364b1b72264f638ec3480e020de8228e7897927
Jack <dlwxxxdlw@JackdeMacBook-Pro.local>
2024-07-23 17:07:56 +0800

    add a run function for easy use

--- sadtalker_video_lip_sync/inference.py
+++ sadtalker_video_lip_sync/inference.py
@@ -16,6 +16,96 @@ from sadtalker_video_lip_sync.third_part.GPEN.gpen_face_enhancer import FaceEnha
 
 warnings.filterwarnings("ignore")
 
+def run(source_video: str, 
+        driven_audio: str, 
+        checkpoint_dir: str,
+        result_dir: str, 
+        enhancer: str = "lip",
+        batch_size: int = 1,
+        use_DAIN: bool = False,
+        dian_output: str = "",
+        DAIN_weight: str = "",
+        time_step: float = 0.05,
+        remove_duplicates: bool = False):
+    pic_path = source_video
+    audio_path = driven_audio
+    enhancer_region = enhancer
+    save_dir = os.path.join(result_dir, strftime("%Y_%m_%d_%H.%M.%S"))
+    os.makedirs(save_dir, exist_ok=True)
+    if torch.cuda.is_available() and not args.cpu:
+        device = "cuda"
+    else:
+        device = "cpu"
+    batch_size = batch_size
+    current_code_path = sys.argv[0]
+    current_root_path = os.path.split(current_code_path)[0]
+    os.environ['TORCH_HOME'] = os.path.join(current_root_path, checkpoint_dir)
+
+    path_of_lm_croper = os.path.join(current_root_path, checkpoint_dir, 'shape_predictor_68_face_landmarks.dat')
+    path_of_net_recon_model = os.path.join(current_root_path, checkpoint_dir, 'epoch_20.pth')
+    dir_of_BFM_fitting = os.path.join(current_root_path, checkpoint_dir, 'BFM_Fitting')
+    wav2lip_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'wav2lip.pth')
+
+    audio2pose_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'auido2pose_00140-model.pth')
+    audio2pose_yaml_path = os.path.join(current_root_path, 'src', 'config', 'auido2pose.yaml')
+
+    audio2exp_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'auido2exp_00300-model.pth')
+    audio2exp_yaml_path = os.path.join(current_root_path, 'src', 'config', 'auido2exp.yaml')
+
+    free_view_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'facevid2vid_00189-model.pth.tar')
+
+    mapping_checkpoint = os.path.join(current_root_path, checkpoint_dir, 'mapping_00109-model.pth.tar')
+    facerender_yaml_path = os.path.join(current_root_path, 'src', 'config', 'facerender_still.yaml')
+
+    # init model
+    print(path_of_net_recon_model)
+    preprocess_model = CropAndExtract(path_of_lm_croper, path_of_net_recon_model, dir_of_BFM_fitting, device)
+
+    print(audio2pose_checkpoint)
+    print(audio2exp_checkpoint)
+    audio_to_coeff = Audio2Coeff(audio2pose_checkpoint, audio2pose_yaml_path, audio2exp_checkpoint, audio2exp_yaml_path,
+                                 wav2lip_checkpoint, device)
+
+    print(free_view_checkpoint)
+    print(mapping_checkpoint)
+    animate_from_coeff = AnimateFromCoeff(free_view_checkpoint, mapping_checkpoint, facerender_yaml_path, device)
+
+    restorer_model = GFPGANer(model_path='checkpoints/GFPGANv1.3.pth', upscale=1, arch='clean',
+                              channel_multiplier=2, bg_upsampler=None)
+    enhancer_model = FaceEnhancement(base_dir='checkpoints', size=512, model='GPEN-BFR-512', use_sr=False,
+                                     sr_model='rrdb_realesrnet_psnr', channel_multiplier=2, narrow=1, device=device)
+
+    # crop image and extract 3dmm from image
+    first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
+    os.makedirs(first_frame_dir, exist_ok=True)
+    print('3DMM Extraction for source image')
+    first_coeff_path, crop_pic_path, crop_info = preprocess_model.generate(pic_path, first_frame_dir)
+    if first_coeff_path is None:
+        print("Can't get the coeffs of the input")
+        return
+    # audio2ceoff
+    batch = get_data(first_coeff_path, audio_path, device)
+    coeff_path = audio_to_coeff.generate(batch, save_dir)
+    # coeff2video
+    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, device)
+    tmp_path, new_audio_path, return_path = animate_from_coeff.generate(data, save_dir, pic_path, crop_info,
+                                                                        restorer_model, enhancer_model, enhancer_region)
+    torch.cuda.empty_cache()
+    if use_DAIN:
+        import paddle
+
+        from sadtalker_video_lip_sync.src.dain_model import dain_predictor
+        paddle.enable_static()
+        predictor_dian = dain_predictor.DAINPredictor(dian_output, weight_path=DAIN_weight,
+                                                      time_step=time_step,
+                                                      remove_duplicates=remove_duplicates)
+        frames_path, temp_video_path = predictor_dian.run(tmp_path)
+        paddle.disable_static()
+        save_path = return_path[:-4] + '_dain.mp4'
+        command = r'ffmpeg -y -i "%s" -i "%s" -vcodec copy "%s"' % (temp_video_path, new_audio_path, save_path)
+        os.system(command)
+    os.remove(tmp_path)
+
 
 def main(args):
     pic_path = args.source_video
